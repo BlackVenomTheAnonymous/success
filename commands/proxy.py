68e955e8d98030ccf75ab4dd8dcd0b0dd8854052
BlackVenomTheAnonymous <likhons304@gmail.com>
2023-05-22 15:59:53 +0600

    Update proxy.py

--- commands/proxy.py
+++ commands/proxy.py
@@ -1,1 +1,19 @@
+import requests
+from telegram import Update
+from telegram.ext import CallbackContext, CommandHandler
+from free_proxy import FreeProxy
 
+def fetch_proxy(update: Update, context: CallbackContext):
+    # Get a free working proxy
+    proxy = FreeProxy().get()
+
+    if proxy:
+        # Send the proxy information to the user
+        message = f"🔒 Proxy: {proxy}"
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+    else:
+        message = "Failed to fetch a proxy 😔"
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
+def setup(dispatcher):
+    dispatcher.add_handler(CommandHandler("proxy", fetch_proxy))
